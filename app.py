f64f9a86d42097f7ed4e4887d13919b539c5ef04
fxKatsumi <takahata@forex.co.jp>
2023-06-13 16:09:47 +0900

    結果テキスト表示

--- app.py
+++ app.py
@@ -1,90 +1,106 @@
-import shutil
-import sys
-import os
-
-from PIL import ImageDraw, Image, ImageFont
+import requests
+from io import BytesIO
+from PIL import Image, ImageDraw, ImageFont
+import time
 import streamlit as st
+import sys
 
-def font_installed_dirs() -> list[str]:
-    """
-    インストールされているフォントのディレクトリ取得
-    """
+# フォント
+# Windows
+font_name_win = "./meiryo.ttc" # メイリョウ
+# Mac
+font_name_mac = "ヒラギノ丸ゴ ProN W4.ttc"
+# Linux
+font_name_lnx = "DejaVuSerif.ttf"
 
-    if sys.platform == "win32":
-        d = os.environ.get("WINDIR")
-        return [os.path.join(d, "fonts")]
+# フォント
+if sys.platform == "win32": # Windows
+    font_name = font_name_win
+if sys.platform == "darwin": # Mac
+    font_name = font_name_mac
+if sys.platform in ("linux", "linux2"): # Linux
+    font_name = font_name_lnx
 
-    if sys.platform in ("linux", "linux2"):
-        dirs = os.environ.get("XDG_DATA_DIRS", "")
-        if not dirs:
-            dirs = "/usr/share"
-        return [os.path.join(d, "fonts") for d in dirs.split(":")]
+# ファイルをアップロードするためのStreamlitのウィジェットを表示
+uploaded_file = st.file_uploader("名刺の画像ファイルをアップロードしてください", type=["jpg", "jpeg", "png"])
 
-    if sys.platform == "darwin":
-        return ["/Library/Fonts",
-                "/System/Library/Fonts",
-                os.path.expanduser("~/Library/Fonts")]
+if uploaded_file is not None:
+    # 画像ファイルを読み込む
+    im = Image.open(uploaded_file)
+    imFilePath = BytesIO(uploaded_file.getvalue())
 
-    raise Exception(f"unsupported platform:{sys.platform}")
+    # Azure Computer Vision API の endpoint を指定する
+    endpoint = "https://computervisiontestm01.cognitiveservices.azure.com/"
+    text_recognition_url = endpoint + "vision/v3.2/read/analyze"
 
-def font_path_list() -> list[str]:
-    """
-    インストールされているフォントのPATHリストを取得
-    """
+    # ヘッダー情報を指定する
+    headers = {
+        'Ocp-Apim-Subscription-Key': st.secrets.azure_settings.azure_subscription_key,
+        'Content-Type': 'application/octet-stream'
+    }
+    params = {
+        'language': 'ja',
+        'model-version': 'latest'
+    }
 
-    # フォントがインストールされているディレクトリリスト取得
-    dirs = font_installed_dirs()
+    # 指定した画像の read メソッドを呼び出します。これによって operation ID が返され、画像の内容を読み取る非同期プロセスが開始されます
+    response = requests.post(text_recognition_url, headers=headers, params=params, json=None, data=imFilePath)
+    response.raise_for_status()
 
-    # os.walkを使って、ファイル一覧を取得する
-    l: list[str] = []
-    for d in dirs:
-        for parent, _, filenames in os.walk(d):
-            for name in filenames:
-                l.append(os.path.join(parent, name))
+    # レスポンスから operation location（末尾にIDが付いたURL）を取得する
+    operation_url = response.headers["Operation-Location"]
+    analysis = {}
+    poll = True
 
-    return l
+    # read の呼び出しから返された operation location ID を取得し、操作の結果をサービスに照会します。
+    # 次のコードは、結果が返されるまで 1 秒間隔で操作をチェックします
+    while poll:
+        response_final = requests.get(operation_url, headers=headers)
+        analysis = response_final.json()
 
-def show_fonts():
-    """
-    フォント毎のサンプル画像を出力します。
-    """
-    font_size = 28
+        if "analyzeResult" in analysis:
+            poll = False
+        if "status" in analysis and analysis['status'] == 'failed':
+            poll = False
 
-    # # 出力ディレクトリの再作成
-    # output_dir = os.path.join("out", "font_samples")
-    # shutil.rmtree(output_dir)
-    # os.mkdir(output_dir)
+        if poll:
+            time.sleep(1)
 
-    flist = ""
+    # 画像イメージ読み込み
+    draw = ImageDraw.Draw(im)
+    fnt = ImageFont.truetype(font_name, 15)
 
-    for f in font_path_list():
-        font_name = os.path.basename(f)
-        # image = Image.new('RGBA', (600, 200), 'white')
-        # draw = ImageDraw.Draw(image)
+    txt_lines = ""
 
-        # print(font_name)
-        flist += font_name + "\n"
+    if "analyzeResult" in analysis:  # 成功した場合
+        # 行ごとにデータを格納する
+        lines = [(line["boundingBox"], line["text"], line["words"])
+                 for line in analysis["analyzeResult"]["readResults"][0]["lines"]]
 
-        # try:
-        #     font = ImageFont.truetype(f, font_size)
-        #     draw.text((0, 0),
-        #               f"{font_name}\nサンプルのテキストを表示\nfont_size:{font_size}",
-        #               font=font,
-        #               fill='black')
-        # except OSError as err:
-        #     # エラーがでた場合、スキップ
-        #     print(f"failed to draw with font:{f} caused by {err}", file=sys.stderr)
-        #     continue
+        # 行レベル
+        for line in lines:
+            boundingBox = line[0]
+            text = line[1]
+            words = line[2]
 
-        # # 画像保存
-        # image.save(os.path.join(output_dir, font_name + ".png"), "PNG")
+            txt_lines += text + "\n"
 
-        # # パスをテキストで保存
-        # with open(os.path.join(output_dir, font_name + ".txt"), "w") as fp:
-        #     fp.write(f"{f}\n")
-        #     fp.write(f"{font_name}\n")
+            # 文字レベル
+            for word in words:
+                try:
+                    wp = word["boundingBox"]
+                    wtext = word["text"]
+                    wconfidence = word['confidence']
 
-    st.text(flist)
+                    # 矩形を描画
+                    draw.rectangle([wp[0], wp[1], wp[4], wp[5]], fill=None, outline=(0, 255, 0), width=1)
+                    # テキストを描画
+                    draw.text((wp[6] + 5, wp[7] - 5), wtext, font=fnt, fill=(0, 0, 255))
+                except:
+                    pass
 
-if __name__ == '__main__':
-    show_fonts()
+    # 画像を表示
+    st.image(im, caption='OCR 結果の画像', use_column_width=True)
+
+    # テキストを表示
+    st.text_area("OCR 結果のテキスト", value=txt_lines, height=200)
